ececeac03a0110b91b3aeaa60c89b7dfb9167c2f
Tenzin Lakshay <tenzinlakshay340@gmail.com>
2026-04-25 01:37:54 +0530

    gcgfhnf

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 import requests
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return send_file("index.html")
 
 @app.route('/precool', methods=['GET'])
 def precool():
@@ -19,16 +23,25 @@ def precool():
 
         tomorrow = (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d')
         pre_cool = False
+        first_temp = None
         
         for item in data['list']:
             dt = item['dt_txt']
+            if first_temp is None:
+                first_temp = item['main']['temp']
+
             if tomorrow in dt:
                 temp = item['main']['temp']
                 hour = int(dt.split(' ')[1].split(':')[0])
                 if 8 <= hour <= 11 and temp > 35:
                     pre_cool = True
                 
-        return jsonify({"pre_cooling": pre_cool, "city": CITY, "tomorrow": tomorrow})
+        return jsonify({
+            "pre_cooling": pre_cool,
+            "city": CITY,
+            "tomorrow": tomorrow,
+            "external_temp": first_temp
+        })
     except Exception as e:
         return jsonify({"error": str(e)})
 
